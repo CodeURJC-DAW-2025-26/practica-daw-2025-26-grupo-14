98014486bb11b9a05f2526162f9dd23bd43e56f3
A-Trigue <a.trigueror.2020@alumnos.urjc.es>
2026-02-24 09:12:33 +0100

    Footer & Header

--- src/main/java/es/codeurjc/daw/library/model/Product.java
+++ src/main/java/es/codeurjc/daw/library/model/Product.java
@@ -8,11 +8,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-//import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-//import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="products")
 public class Product {
 
 	@Id
--- src/main/java/es/codeurjc/daw/library/model/User.java
+++ src/main/java/es/codeurjc/daw/library/model/User.java
@@ -2,15 +2,15 @@ package es.codeurjc.daw.library.model;
 
 import java.util.List;
 
-//import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-//import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="users")
 public class User {
 
 	@Id
