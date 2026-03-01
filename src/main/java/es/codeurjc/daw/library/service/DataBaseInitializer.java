2d119e3945102f7e14de620303736dc54403c326
SaraTuset <117302443+SaraTuset@users.noreply.github.com>
2026-03-01 21:16:53 +0100

    Save images in DB

--- src/main/java/es/codeurjc/daw/library/service/DataBaseInitializer.java
+++ src/main/java/es/codeurjc/daw/library/service/DataBaseInitializer.java
@@ -4,15 +4,19 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import es.codeurjc.daw.library.model.Image;
 import es.codeurjc.daw.library.model.Product;
 import es.codeurjc.daw.library.model.User;
 import es.codeurjc.daw.library.repository.ProductRepository;
 import es.codeurjc.daw.library.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 //import es.codeurjc.daw.library.repository.UserRepository;
+
 
 @Service
 public class DataBaseInitializer {
@@ -29,6 +33,9 @@ public class DataBaseInitializer {
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private ImageService imageService;
+
 	@PostConstruct
 	public void init() throws IOException, URISyntaxException {
         if (userRepository.count() > 0 || productRepository.count() > 0) {
@@ -40,6 +47,10 @@ public class DataBaseInitializer {
         User seller3 =  new User("Carlos Lopez", "Carlos", "carlos@lopez.com", "Valencia", passwordEncoder.encode("pass"), "USER");
         User admin =  new User("Admin User", "Admin", "admin@user.com", "Sevilla", passwordEncoder.encode("adminpass"), "USER", "ADMIN");
 
+        seller1.setImage(setImage("/static/images/Mario_profile.jpeg"));
+        seller2.setImage(setImage("/static/images/LuGar_profile.webp"));
+        seller3.setImage(setImage("/static/images/Carlos_profile.webp"));
+        admin.setImage(setImage("/static/images/admin_profile.webp"));
 
         userRepository.save(seller1);
 		userRepository.save(seller2);
@@ -49,32 +60,34 @@ public class DataBaseInitializer {
         // Sample Products
 		Product prod1 = new Product(
             "Camisa de manga larga", seller1, 15, "Ropa",
-            "New", "Camisa de manga larga en buen estado, talla M.", null, "Phone"
+            "New", "Camisa de manga larga en buen estado, talla M.",  "Phone"
         );
+        prod1.setImage(setImage("/static/images/camisa.webp"));
         productService.save(prod1);
 
         Product prod2 = new Product(
             "Bicicleta de montanna", seller2, 120, "Deportes", "Acceptable",
-            "Bicicleta de montanna en buen estado, con cambios y frenos funcionales.", null, "Chat"
+            "Bicicleta de montanna en buen estado, con cambios y frenos funcionales.",  "Chat"
         );
+        prod2.setImage(setImage("/static/images/bicicleta.webp"));
         productService.save(prod2);
         Product prod3 = new Product(
             "Sofa de dos plazas", seller3, 250, "Hogar", "Like new",
-            "Sofa de dos plazas en buen estado, color gris.", null, "Both"
+            "Sofa de dos plazas en buen estado, color gris.",  "Both"
         );
+        prod3.setImage(setImage("/static/images/sofa.webp"));
         productService.save(prod3);
 
 
 		// Sample users
 
-		/*userRepository.save(new User("user", passwordEncoder.encode("pass"), "USER"));
-		userRepository.save(new User("admin", passwordEncoder.encode("adminpass"), "USER", "ADMIN"));
+		
 	}
 
-	public void setBookImage(Book book, String classpathResource) throws IOException {
+	public Image setImage(String classpathResource) throws IOException {
 		Resource image = new ClassPathResource(classpathResource);
 
 		Image createdImage = imageService.createImage(image.getInputStream());
-		book.setImage(createdImage);*/
+		return createdImage;
 	}
 }
