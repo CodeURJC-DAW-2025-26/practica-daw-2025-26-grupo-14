0d4d87f9ccdb7ad3df5089a4782e203f9d2e80cd
A-Trigue <a.trigueror.2020@alumnos.urjc.es>
2026-03-02 02:49:04 +0100

    User Validation

--- src/main/java/es/codeurjc/daw/library/model/User.java
+++ src/main/java/es/codeurjc/daw/library/model/User.java
@@ -13,6 +13,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name="users")
@@ -23,14 +27,23 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id = null;
 
+	@NotBlank(message = "full name is required")
+	@Size(min = 2 , max = 65, message = "full name must be between 2 an 65 characters")
 	private String fullName;
 
+	@NotBlank(message = "username is required")
+	@Size(min = 2 , max = 20, message = "username must be between 2 an 20 characters")
+	@Pattern(regexp = "^[a-zA-Z0-9]+$", message = "username can only contain letters, numbers and underscores")
     private String name;
 
-    private String email;
+	@NotBlank(message = "email is required")
+	@Email(message = "email should be valid")
+	private String email;
 
     private String city;
 
+	@NotBlank(message = "password is required")
+	@Size(min = 6, message = "password must be at least 6 characters long")
 	private String password;
 
 	@ElementCollection(fetch = FetchType.EAGER)
