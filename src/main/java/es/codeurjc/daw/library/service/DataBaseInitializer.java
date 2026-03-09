dff46f64c717af19382e47cf1528e1f72cecc526
SaraTuset <117302443+SaraTuset@users.noreply.github.com>
2026-03-10 00:25:32 +0100

    Little image fix

--- src/main/java/es/codeurjc/daw/library/service/DataBaseInitializer.java
+++ src/main/java/es/codeurjc/daw/library/service/DataBaseInitializer.java
@@ -47,10 +47,10 @@ public class DataBaseInitializer {
         User seller3 =  new User("Carlos Lopez", "Carlos", "carlos@lopez.com", "Valencia", passwordEncoder.encode("pass"), "12345678C", "USER");
         User admin =  new User("Admin User", "Admin", "admin@user.com", "Sevilla", passwordEncoder.encode("adminpass"), "12345678D", "USER", "ADMIN");
 
-        seller1.setImage(setImage("/static/images/Mario_profile.jpeg"));
-        seller2.setImage(setImage("/static/images/LuGar_profile.webp"));
-        seller3.setImage(setImage("/static/images/Carlos_profile.webp"));
-        admin.setImage(setImage("/static/images/admin_profile.webp"));
+        seller1.setImage(setImage("/static/my_images/Mario_profile.jpeg"));
+        seller2.setImage(setImage("/static/my_images/LuGar_profile.webp"));
+        seller3.setImage(setImage("/static/my_images/Carlos_profile.webp"));
+        admin.setImage(setImage("/static/my_images/admin_profile.webp"));
 
         userRepository.save(seller1);
 		userRepository.save(seller2);
@@ -62,20 +62,20 @@ public class DataBaseInitializer {
             "Camisa de manga larga", seller1, 15, "Ropa",
             "New", "Camisa de manga larga en buen estado, talla M.",  "Phone"
         );
-        prod1.setImage(setImage("/static/images/camisa.webp"));
+        prod1.setImage(setImage("/static/my_images/camisa.webp"));
         productService.save(prod1);
 
         Product prod2 = new Product(
             "Bicicleta de montanna", seller2, 120, "Deportes", "Acceptable",
             "Bicicleta de montanna en buen estado, con cambios y frenos funcionales.",  "Chat"
         );
-        prod2.setImage(setImage("/static/images/bicicleta.webp"));
+        prod2.setImage(setImage("/static/my_images/bicicleta.webp"));
         productService.save(prod2);
         Product prod3 = new Product(
             "Sofa de dos plazas", seller3, 250, "Hogar", "Like new",
             "Sofa de dos plazas en buen estado, color gris.",  "Both"
         );
-        prod3.setImage(setImage("/static/images/sofa.webp"));
+        prod3.setImage(setImage("/static/my_images/sofa.webp"));
         productService.save(prod3);
 
 
