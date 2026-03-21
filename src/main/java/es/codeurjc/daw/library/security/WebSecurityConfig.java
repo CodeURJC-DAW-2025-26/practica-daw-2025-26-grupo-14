77e5753d582e34cc1c69df518d8bdf1c6ee09c1e
SaraTuset <117302443+SaraTuset@users.noreply.github.com>
2026-03-21 14:12:18 +0100

    Added jwt

--- src/main/java/es/codeurjc/daw/library/security/WebSecurityConfig.java
+++ src/main/java/es/codeurjc/daw/library/security/WebSecurityConfig.java
@@ -3,8 +3,10 @@ package es.codeurjc.daw.library.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,6 +25,12 @@ public class WebSecurityConfig {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
+
 
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
