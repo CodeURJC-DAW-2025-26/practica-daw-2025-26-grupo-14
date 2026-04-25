f0b90d78a28105f2ff17ecdbfc714792cb4b0abc
A-Trigue <a.trigueror.2020@alumnos.urjc.es>
2026-04-25 03:52:54 +0200

    Working React

---
+++ frontend/src/main.tsx
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <h1>React funcionando 🎉</h1>
+  </StrictMode>
+)
